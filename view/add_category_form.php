f6c5b5ed3e6e4db2103f202f4c9630c2ff800575
Someone <rgbjkbs@hdbf.ru>
2018-11-19 15:55:14 +0200

    add category form

---
+++ view/add_category_form.php
@@ -0,0 +1,5 @@
+<form  method="post">
+            <label> Наименование категории: <input type="text" name="category"/></label>
+            <input type="hidden" name="action" value="addcategory">
+            <input type="submit" value="Добавить товар"/>
+</form>
