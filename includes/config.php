c23d1010135a358a0541164e7f4d9cd16ee35775
user <Global@mail.ua>
2018-11-19 17:06:18 +0200

    edit config.php

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,13 @@
 <?php
-     const LOGIN = 'root';
-     const PASS = '';
-     const HOST = 'localhost';
-     const NAME = 'r2d2_online-store';
+    const LOGIN = 'root';
+    const PASS = '';
+    const HOST = 'localhost';
+    const NAME = 'r2d2_online-store';
+    const IMAGES_DIRECTORY = 'images'.DIRECTORY_SEPARATOR.'galery';
+    const ALLOWED_TYPES = array(
+    'image/jpeg',
+    'image/jpg',
+    'image/png',
+);
+    const MAX_IMAGE_SIZE = 5 * 1024 * 1024;
 
