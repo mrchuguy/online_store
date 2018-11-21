a7e09de572fb159feefcd550023d388284edc303
user <Global@mail.ua>
2018-11-21 17:24:08 +0200

    update_category.php

--- view/update_category.php
+++ view/update_category.php
@@ -1,6 +1,7 @@
 <form  method="post">
             <label> Наименование категории: <input type="text" name="category" value='<?=$this->category?>'/></label>
             <input type="hidden" name="action" value="update_category_form">
+	    <input type="hidden" name="id" value="<?=$this->id?>">
             <input type="submit" value="Изменить"/>
 </form>
 
