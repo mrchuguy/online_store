cf3d3556bb6e08df0921c39e7e6e3ad4c66dd568
Moesey <45037817+Moesey@users.noreply.github.com>
2018-11-21 16:55:02 +0200

    Update categories.php

--- view/categories.php
+++ view/categories.php
@@ -1,51 +1,23 @@
 <?php
     $obj = new CategoriesModel();
     $categories = $obj->getCategory();
-    // echo '<button>добавить категорию</button>';
-    // echo '<ol>';
-    // foreach($categories as $i=>$category){
-    //     echo '<li>';
-    //     echo '<a href="/admin/admin.php?action=categories&category='.$i.'">'.$category['name'].'</a>';
-    //     echo '<form method="POST">';
-    //     echo '<input type="submit" value="X">';
-    //     echo '<input type="hidden" name="action" value="del_category">';
-    //     echo '<input type="submit" value="edit">';
-    //     echo '</form>';
-    //     echo '<form method="POST">';
-    //     echo '<input type="hidden" name="action" value="update_category">';
-    //     echo '<input type="hidden" name="id" value="'.$i.'">';
-    //     echo '</form>';
-    //     echo '</li>';
-    // }
-    // echo '</ol>';
+    require_once 'add_category_form.php';
 ?>    
 
-<button>добавить категорию</button>
+
 <ol>
 <?php foreach($categories as $category){?>
     <li>
-<<<<<<< HEAD
-        <a href="/admin/admin.php?action=categories&category=<?= $category[$id]?>"><?= $category['name']?></a>
-        <form method="POST">
-            <input type="submit" value="X">
-            <input type="hidden" name="action" value="del_category">
-            <input type="hidden" name="id" value="<?= $category[$id]?>">
-=======
         <a href="/admin/admin.php?action=categories&category=<?= $category['id']?>"><?= $category['name']?></a>
         <form method="POST">
             <input type="submit" value="X">
             <input type="hidden" name="action" value="del_category">
             <input type="hidden" name="id" value="<?= $category['id']?>">
->>>>>>> 8b71fda4f5c76c7618c2ea7026cde833b9e4251e
         </form>
         <form method="POST">
             <input type="submit" value="edit">
             <input type="hidden" name="action" value="update_category">
-<<<<<<< HEAD
-            <input type="hidden" name="id" value="<?= $category[$id]?>">
-=======
             <input type="hidden" name="id" value="<?= $category['id']?>">
->>>>>>> 8b71fda4f5c76c7618c2ea7026cde833b9e4251e
         </form>
     </li>
 <?php }?>
