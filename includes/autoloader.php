c72473bca59f54994cbd041d7c8910f350a556b5
lCassieL <denisandwork@gmail.com>
2018-11-23 11:35:56 +0200

    front part3

--- includes/autoloader.php
+++ includes/autoloader.php
@@ -1,7 +1,8 @@
 <?php
 // include_once '../includes/config.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR.'includes/config.php';
 spl_autoload_register(function ($class) {
-    $path = 'library' . DIRECTORY_SEPARATOR . $class . '.php';
+    $path = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR.'library' . DIRECTORY_SEPARATOR . $class . '.php';
     if (file_exists($path)) {
         include_once $path;
         return TRUE;
