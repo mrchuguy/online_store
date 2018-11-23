381fab38fcf0cee475b8071b468ecd199db854b6
user <user@user.com>
2018-11-23 17:07:25 +0200

    OrdersModel mod

--- library/OrdersModel.php
+++ library/OrdersModel.php
@@ -10,7 +10,7 @@ class OrdersModel {
 
     public function getOrder() {
 	if ($this->db->connect_errno === 0) {
-	    $query = 'select * from orders';
+	    $query = 'select orders.id, comment, status,customers.name,customers.surname,customers.phone,customers.email,status  from  orders LEFT OUTER JOIN customers ON customer_id = customers.id';
 	    $res = $this->db->query($query);
 	    if ($res) {
 		return $res->fetch_all(MYSQLI_ASSOC);
