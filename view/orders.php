09ee569e3e45342575c0adeb05868bf9051c028e
lCassieL <denisandwork@gmail.com>
2018-11-24 17:38:03 +0200

    edit orders

--- view/orders.php
+++ view/orders.php
@@ -3,18 +3,35 @@
     $orders = $obj_orders->getOrder();
 ?>
 <table>
-<?php foreach($orders as $order){?>
-    <tr>
-        <td><?= $order['name'].' '.$order['surname']?></td>
-        <td><?= $order['comment']?></td>
-        <td><?= $order['status']?></td>
-        <td>
-            <form method="post">
-                <input type="submit" value="add to processed">
-                <input type="hidden" name="action" value="change_status_order">
-                <input type="hidden" name="id" value="<?= $order['id']?>"
-            </form>
-        </td>
-    </tr>
-<?php }?>
+<?php 
+    foreach($orders as $order){
+        if($order['status']==='not processed'){
+?>
+            <tr>  
+                <td><?= $order['name'].' '.$order['surname']?></td>
+                <td><?= $order['comment']?></td>
+                <td><?= $order['status']?></td>
+                <td>
+                    <form method="post">
+                        <input type="submit" value="add to processed">
+                        <input type="hidden" name="action" value="change_status_order">
+                        <input type="hidden" name="id" value="<?= $order['id']?>"
+                    </form>
+                </td>
+            </tr>
+<?php
+        }
+    }
+    foreach($orders as $order){
+        if($order['status']==='processed'){
+?>
+            <tr>  
+                <td><?= $order['name'].' '.$order['surname']?></td>
+                <td><?= $order['comment']?></td>
+                <td><?= $order['status']?></td>
+            </tr>
+<?php
+        }
+    }
+?>
 </table>
