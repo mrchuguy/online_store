caabdbff92b6559f1f973c75666e58589b56c502
Vasyl <admin@gmail.com>
2018-11-26 17:08:18 +0200

    change view/goods.php

--- view/goods.php
+++ view/goods.php
@@ -1,13 +1,24 @@
-<?php
-include_once '../includes/autoloader.php';
-$goods_model = new Goods_model();
-$goods = $goods_model->getGoods('1');
-var_dump($goods);
-?>
-
-<input type="submit" value="Добавить товар" name="add_good"/>
-<table border="solid 1">
-    
+<fieldset><p>Добавить товар в категорию <?= $this->category_id ?> </p><div id="add_goods_form">
+        <form id="add goods" method="post" enctype="multipart/form-data">
+            <label> Наименование товара: <input type="text" name="name"/></label>
+            <label> Цена: <input type="number" name="price"/></label>
+            <label> Описание: <textarea name="description"></textarea></label>
+            <label> Выберите производителя: <select id="manufacturers" name="manufacturer">
+		    <?php foreach ($this->manufacturers as $manufacturer) : ?>
+                        <option value="<?= $manufacturer['id'] ?>"><?= $manufacturer['name'] ?></option> 
+		    <?php endforeach; ?>
+                </select></label>
+            <label> Выберите страну производства: <select id="countries" name="country">
+		    <?php foreach ($this->countries as $country) : ?>
+                        <option value="<?= $country['id'] ?>"><?= $country['name'] ?></option> 
+		    <?php endforeach; ?>
+                </select></label>
+            <label> Добавить картинку: <input type="file" name="image[]" accept="image/*" multiple/></label>
+            <input type="submit" value="Добавить товар"/>
+        </form>
+    </div>
+</fieldset>
+<table border="solid 1">    
     <tr>
         <th>Наименование товара</th>
         <th>Цена</th>
@@ -17,7 +28,7 @@ var_dump($goods);
         <th>Категория</th>
         <th>Действие</th>
     </tr>
-    <?php foreach($goods as $good){?>
+    <?php foreach($this->goods as $good){?>
     <tr>
         <td>
             <?= $good['name']?>
